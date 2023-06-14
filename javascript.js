ac332a5218428c713793390c8cab3433ecf79dd8
oyupanqui <osmar30asis@gmail.com>
2023-06-13 22:32:41 -0500

    solved reset and alert

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 let gameMode;
+
+function vs(elem) {
+    gamemode = elem
+    resetGame()
+}
+
 const cells = document.getElementsByClassName("cell")
 const left = document.getElementsByClassName("left-panel")
 const right = document.getElementsByClassName("right-panel")
@@ -7,6 +13,21 @@ let play = 0
 
 let P1 = []
 let P2 = []
+
+const winCon = [["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"],
+    ["11", "21", "31"], ["12", "22", "32"], ["13", "23", "33"],
+    ["11", "22", "33"], ["13", "22", "31"]]
+
+function resetGame() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = ''
+        left[0].innerHTML = "Player One's turn"
+        P1 = []
+        P2 = []
+        play = 0
+        cells[i].setAttribute("onClick", "choice(event)")
+    }
+}
 
 function choice (event) {
     right[0].innerHTML = ''
@@ -37,34 +58,17 @@ function checkWin () {
     for (let i = 0; i<winCon.length; i++) {
         if (winCon[i].every(r => P1.includes(r))) {
             right[0].innerHTML = ''
-            left[0].innerHTML = ''
-            alert('P1 wins')
-            resetGame()
+            left[0].innerHTML = 'Player One wins!'
+            setTimeout(() => {
+                resetGame()
+            }, 2000)
+            
         } else if (winCon[i].every(r => P2.includes(r))) {
-            right[0].innerHTML = ''
+            right[0].innerHTML = 'Player Two wins!'
             left[0].innerHTML = ''
-            alert('P2 wins')
-            resetGame()
+            setTimeout(() => {
+                resetGame()
+            }, 2000)
         }
     }
-}
-
-const winCon = [["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"],
-    ["11", "21", "31"], ["12", "22", "32"], ["13", "23", "33"],
-    ["11", "22", "33"], ["13", "22", "31"]]
-
-function resetGame() {
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].innerHTML = ''
-        left[0].innerHTML = "Player One's turn"
-        P1 = []
-        P2 = []
-        play = 0
-        cells[i].setAttribute("onClick", "choice(event)")
-    }
-}
-
-function vs(elem) {
-    gamemode = elem
-    resetGame()
 }
